32c2e8912d660aff91d800f26124f2a68e660721
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-02 18:19:50 +0530

    ADDED: Calculate_Daily_Employee_Wage

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -1,17 +1,25 @@
 package com.bridgelabz;
 
+/**
+ * 
+ * // Calculate Dailly Employee Wage
+ *
+ */
 public class Emp_Wage {
 	public static void main(String[] args) {
 		// constants
 		int IS_FULL_TIME = 1;
-		// Computation
+		int EMP_RATE_PER_HOUR = 20;
+		// Declared and Initialize variables
+		int empHrs = 0, empWage = 0;
+		// computations
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
-		} else {
-			System.out.println("Employee is Absent");
-		}
-
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("EmpWage: " + empWage);
 	}
 
 }
