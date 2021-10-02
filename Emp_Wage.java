4ace04bc74f5262965598e01746dac79f6dd027d
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-02 18:37:04 +0530

    ADDED: Calculating_Wages_For_A_Month

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 /**
  * 
- * // Solving Using Switch Case Statement
+ * // Calculating Wages For a Month
  *
  */
 public class Emp_Wage {
@@ -11,23 +11,28 @@ public class Emp_Wage {
 		final int IS_PART_TIME = 1;
 		final int IS_FULL_TIME = 2;
 		final int EMP_RATE_PER_HOUR = 20;
+		final int NUM_OF_WORKING_DAYS = 20;
 
 		// initializing variable
-		int empWage = 0, empHrs = 0;
-		// computations
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3; // type casting(int) all belong to Number family
-		switch (empCheck) {
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-		default:
-			empHrs = 0;
+		int empHrs = 0, empWage = 0, totalEmpWage = 0;
+		// computation
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage: " + empWage);
+			totalEmpWage += empWage;
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 
 }
