ef009fc9fc2044580a95f94124aa9e4162248a2c
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-02 18:31:49 +0530

    ADDED: Solving_Using_Switch_Case_Statement

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -2,26 +2,30 @@ package com.bridgelabz;
 
 /**
  * 
- * // Add Part Time Employee & Wages
+ * // Solving Using Switch Case Statement
  *
  */
 public class Emp_Wage {
 	public static void main(String[] args) {
-		// constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
-		// variables
-		int empHrs = 0;
-		int empWage = 0;
+		// introducing variable as a constant
+		final int IS_PART_TIME = 1;
+		final int IS_FULL_TIME = 2;
+		final int EMP_RATE_PER_HOUR = 20;
+
+		// initializing variable
+		int empWage = 0, empHrs = 0;
 		// computations
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3; // type casting(int) all belong to Number family
+		switch (empCheck) {
+		case IS_PART_TIME:
 			empHrs = 4;
-		else
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
 			empHrs = 0;
+		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage: " + empWage);
 	}
