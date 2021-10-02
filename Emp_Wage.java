7475d56d3712f457da7f56aa7db082e48ec43d52
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-02 18:24:09 +0530

    ADDED: Add_Part_Time_Employee_And_Wage

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -2,24 +2,28 @@ package com.bridgelabz;
 
 /**
  * 
- * // Calculate Dailly Employee Wage
+ * // Add Part Time Employee & Wages
  *
  */
 public class Emp_Wage {
 	public static void main(String[] args) {
 		// constants
-		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
-		// Declared and Initialize variables
-		int empHrs = 0, empWage = 0;
+		// variables
+		int empHrs = 0;
+		int empWage = 0;
 		// computations
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
 		else
 			empHrs = 0;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("EmpWage: " + empWage);
+		System.out.println("Emp Wage: " + empWage);
 	}
 
 }
